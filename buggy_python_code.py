db9ff25836a1dc6e54c5dba8c702670ce05919a4
Michael Koudela <xkoudel2@fi.muni.cz>
2023-03-28 18:51:06 +0200

    Fix random errors

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -1,4 +1,3 @@
-import sys 
 import os
 import yaml
 import flask
@@ -14,7 +13,7 @@ def index():
 
         
 CONFIG = {"API_KEY": "771df488714111d39138eb60df756e6b"}
-class Person():
+class Person:
     def __init__(self, name):
         self.name = name
 
